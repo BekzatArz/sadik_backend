498f07e2f418e909d2e77d4acb475607b062878d
Beksii <heliumvalley@gmail.com>
2025-02-18 00:33:36 +0600

    change commit

--- app/config.py
+++ app/config.py
@@ -1,8 +1,9 @@
 class Config:
-    DB_HOST = "127.0.0.1"
-    DB_USER = "postgres"
-    DB_PASSWORD = "postgres"
-    DB_NAME = "postgres"
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+    # Используем строку подключения, которую вам дал Railway
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:LgRnVpYsugrREeFFFuGBRCrRhIjiGyOP@postgres-1twg.railway.internal:5432/railway"
+    
+    # Отключаем отслеживание изменений объектов SQLAlchemy
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Секретный ключ для обеспечения безопасности в приложении
     SECRET_KEY = 'BexBexBex'
