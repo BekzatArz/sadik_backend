3abfa5faeb6ac5d4df60dfc2cfb04ad9ca285f7f
Beksii <heliumvalley@gmail.com>
2025-02-20 10:42:36 +0600

    change commit

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 class Config:
-    DB_HOST = "postgres-1twg.railway.internal:5432"
+    DB_HOST = "127.0.0.1"
     DB_USER = "postgres"
-    DB_PASSWORD = "LgRnVpYsugrREeFFFuGBRCrRhIjiGyOP"
-    DB_NAME = "railway"
+    DB_PASSWORD = "postgres"
+    DB_NAME = "postgres"
     SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'BexBexBex'
+    SECRET_KEY = 'BexBexBex' 
