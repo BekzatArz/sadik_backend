8aeb8292a1a2b35479a60896032cff4dc86f6e6c
Beksii <heliumvalley@gmail.com>
2025-02-20 11:59:23 +0600

    change commit

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 from app import create_app
 from flask_migrate import upgrade
+import os
 
 app = create_app()
 
-
-with app.app_context():
-    upgrade() 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    with app.app_context():
+        upgrade()  # Выполняем миграции
+
+    port = int(os.environ.get("PORT", 80))  # Берём порт из окружения (если есть)
+    app.run(host="0.0.0.0", port=port)
