ea4cf6b59f3bc4f31ed79284ec32ffd866ac7ea8
Beksii <heliumvalley@gmail.com>
2025-02-20 10:44:54 +0600

    change commit

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 class Config:
     DB_HOST = "127.0.0.1"
-    DB_USER = "postgres"
-    DB_PASSWORD = "postgres"
-    DB_NAME = "postgres"
+    DB_USER = "bexy"
+    DB_PASSWORD = "Beka2007"
+    DB_NAME = "sadik"
     SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'BexBexBex' 
