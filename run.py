33488641321e0c6d9d498be71c36f097b2457709
Beksii <heliumvalley@gmail.com>
2025-03-05 15:50:40 +0600

    change commit

--- run.py
+++ run.py
@@ -5,5 +5,8 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
+    with app.app_context():
+        upgrade()  # Выполняем миграции
+
     port = int(os.environ.get("PORT", 80))  # Берём порт из окружения (если есть)
     app.run(host="0.0.0.0", port=port)
