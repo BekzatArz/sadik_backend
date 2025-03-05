d230391c76720e637f627291a2e4939b42822088
Beksii <heliumvalley@gmail.com>
2025-03-05 16:04:04 +0600

    change commit

--- app/config.py
+++ app/config.py
@@ -1,19 +1,19 @@
-# class Config:
-#     DB_HOST = "127.0.0.1"
-#     DB_USER = "postgres"
-#     DB_PASSWORD = "postgres"
-#     DB_NAME = "postgres"
-#     SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-#     SECRET_KEY = 'BexBexBex' 
-
-
-
 class Config:
-    DB_HOST = "amvera-bexslay-cnpg-flaskdb-rw"
-    DB_USER = "bexy"
-    DB_PASSWORD = "Beka2007"
-    DB_NAME = "sadik"
+    DB_HOST = "127.0.0.1"
+    DB_USER = "postgres"
+    DB_PASSWORD = "postgres"
+    DB_NAME = "postgres"
     SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'BexBexBex' 
+
+
+
+# class Config:
+#     DB_HOST = "amvera-bexslay-cnpg-flaskdb-rw"
+#     DB_USER = "bexy"
+#     DB_PASSWORD = "Beka2007"
+#     DB_NAME = "sadik"
+#     SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     SECRET_KEY = 'BexBexBex' 
