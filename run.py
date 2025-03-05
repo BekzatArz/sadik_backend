4b5bde82d1bc3d8913fb82d8c2919038e5b508bb
Beksii <heliumvalley@gmail.com>
2025-03-05 14:24:14 +0600

    change commit

--- run.py
+++ run.py
@@ -1,12 +1,23 @@
 from app import create_app
-from flask_migrate import upgrade
+from flask_migrate import upgrade, migrate, init
 import os
 
 app = create_app()
 
 if __name__ == "__main__":
     with app.app_context():
-        upgrade()  # Выполняем миграции
+        try:
+            # Инициализация папки с миграциями (если её нет)
+            if not os.path.exists("migrations"):
+                init()
 
-    port = int(os.environ.get("PORT", 80))  # Берём порт из окружения (если есть)
+            # Создание новых миграций
+            migrate()
+
+            # Применение миграций к БД
+            upgrade()
+        except Exception as e:
+            print(f"Ошибка при миграции: {e}")
+
+    port = int(os.environ.get("PORT", 80))
     app.run(host="0.0.0.0", port=port)
