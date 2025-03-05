d220d505779116a6281df885b10b40a2a864efc8
Beksii <heliumvalley@gmail.com>
2025-03-05 15:46:24 +0600

    change commit

--- run.py
+++ run.py
@@ -1,16 +1,9 @@
 from app import create_app
-from app.extensions import db
+from flask_migrate import upgrade
 import os
 
 app = create_app()
 
-# Функция для создания таблиц в базе данных
-def create_tables():
-    with app.app_context():
-        db.create_all()  # Создаем все таблицы в базе данных
-
 if __name__ == "__main__":
-    create_tables()  # Выполняем создание таблиц
-
     port = int(os.environ.get("PORT", 80))  # Берём порт из окружения (если есть)
     app.run(host="0.0.0.0", port=port)
