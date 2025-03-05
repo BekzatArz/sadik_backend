68d5fe4f342aa8bb63aa6208ac9247e78e178f8b
Beksii <heliumvalley@gmail.com>
2025-03-05 15:14:54 +0600

    change commit

--- run.py
+++ run.py
@@ -1,25 +1,19 @@
-from app import create_app, db
-from flask_migrate import upgrade, migrate, init
+from app import create_app
+from flask_migrate import upgrade, Migrate
+from flask_script import Manager
+from app.extensions import db
 import os
 
 app = create_app()
+manager = Manager(app)
+manager.add_command('db', Migrate)
+
+# Создание миграций
+manager.run('db migrate')
 
 if __name__ == "__main__":
     with app.app_context():
-        try:
-            # Проверяем, есть ли папка миграций
-            if not os.path.exists("migrations"):
-                init()  # Создаём папку с миграциями
+        upgrade()  # Выполняем миграции
 
-            migrate()  # Генерируем миграции
-            upgrade()  # Применяем миграции
-            print("✅ Миграции успешно применены!")
-
-            # Проверяем, есть ли таблица users
-            result = db.session.execute("SELECT * FROM users LIMIT 1;")
-            print("✅ Таблица users найдена!")
-        except Exception as e:
-            print(f"❌ Ошибка при миграции: {e}")
-
-    port = int(os.environ.get("PORT", 80))
+    port = int(os.environ.get("PORT", 80))  # Берём порт из окружения (если есть)
     app.run(host="0.0.0.0", port=port)
