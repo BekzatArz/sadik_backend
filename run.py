18187c3636a862f78ec10d60bc922dd4003f4bb2
Beksii <heliumvalley@gmail.com>
2025-03-06 16:52:51 +0600

    change commit

--- run.py
+++ run.py
@@ -1,20 +1,40 @@
 from app import create_app
-from flask_migrate import upgrade
-from migrate_script import run_migrations
+from flask_migrate import upgrade, Migrate, migrate, init
+
 from app.extensions import db
+import subprocess
 import os
 
 app = create_app()
-@app.route('/tables')
-def list_tables():
-    # Получаем список таблиц
+migrate = Migrate(app, db)
+
+def run_migrations():
     with app.app_context():
-        tables = db.engine.table_names()  # Получаем все таблицы из базы данных
-        return {"tables": tables}
+        # Проверяем, существует ли папка миграций, если нет, то инициализируем
+        if not os.path.exists("migrations"):
+            print("Папка миграций не существует. Инициализация...")
+            init()
+
+        # Получаем все таблицы в базе данных
+        existing_tables = db.engine.table_names()
+
+        # Проверка, если таблицы уже существуют, выполняем миграцию
+        if existing_tables:
+            print("Таблицы уже существуют. Проверка на изменения...")
+
+            # Запуск миграции, если изменения есть
+            subprocess.run(["flask", "db", "migrate", "-m", "Auto migration"], check=True)
+
+            # Применение миграций, если они были созданы
+            subprocess.run(["flask", "db", "upgrade"], check=True)
+            print("Миграции успешно применены.")
+        else:
+            # Если таблиц нет, создаем их через db.create_all()
+            print("Таблицы не найдены, создание через db.create_all()...")
+            db.create_all()
+            print("Таблицы успешно созданы.")
 
 if __name__ == "__main__":
-    with app.app_context():
-        run_migrations()  # Выполняем миграции
-
+    run_migrations()
     port = int(os.environ.get("PORT", 80))  # Берём порт из окружения (если есть)
     app.run(host="0.0.0.0", port=port)
