8ee548f0f40dc79e1e9bb6fbdf8c79a93ecdc4b3
Beksii <heliumvalley@gmail.com>
2025-03-09 22:38:53 +0600

    change commitа

--- run.py
+++ run.py
@@ -1,12 +1,9 @@
 from app import create_app
-from flask_migrate import upgrade, Migrate, migrate, init
-from sqlalchemy import inspect
-from app.extensions import db
 import os
 
 app = create_app()
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Берём порт из окружения (если есть)
+    port = int(os.environ.get("PORT", ))  # Берём порт из окружения (если есть)
     app.run(host="0.0.0.0", port=port)
