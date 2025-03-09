47dc7b9ab0b7fd358d819d194a66e112e74f4963
Beksii <heliumvalley@gmail.com>
2025-03-09 23:57:35 +0600

    change commitа

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Берём порт из окружения (если есть)
+    port = int(os.environ.get("PORT", 80))  # Берём порт из окружения (если есть)
     app.run(host="0.0.0.0", port=port)
