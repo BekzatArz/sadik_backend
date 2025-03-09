3428306ea2a836ebb970936744e6c7688ce67e16
Beksii <heliumvalley@gmail.com>
2025-03-10 00:02:52 +0600

    change commitа

--- run.py
+++ run.py
@@ -1,8 +1,18 @@
 from app import create_app
 import os
+import logging
+
+# Настройка логирования
+logging.basicConfig(
+    filename='/app/app.log',  # Путь до лог-файла в контейнере
+    level=logging.DEBUG,  # Уровень логирования (DEBUG, INFO, WARNING, ERROR)
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'  # Формат записи в лог
+)
+
+# Логирование при старте приложения
+logging.info("Application is starting...")
 
 app = create_app()
-
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 80))  # Берём порт из окружения (если есть)
